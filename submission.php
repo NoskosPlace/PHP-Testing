0b70dfbf575c8528894dafe17ce947ee0bae171b
Wyatt Mack <wyattmack91@gmail.com>
2020-02-18 21:55:17 -0700

    Fix typo in line seven

--- submission.php
+++ submission.php
@@ -4,7 +4,7 @@ print_r('fruit:' . htmlspecialchars($_GET['fruit']) . '<br />');
 print_r('asr:' . htmlspecialchars($_GET['asr']) . '<br /><br /> OR <br /><br />');
 print_r($_GET);
 
-echo '<br /><br />These are the PUT key:value pairs:<br />';
+echo '<br /><br />These are the POST key:value pairs:<br />';
 print_r('show:' . htmlspecialchars($_POST['show']) . '<br />');
 print_r('game:' . htmlspecialchars($_POST['game']) . '<br /><br /> OR <br /><br />');
 print_r($_POST);
