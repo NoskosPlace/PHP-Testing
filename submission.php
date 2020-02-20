2ea7ab4a7e0d634b52ed591f727ef1dc5501048b
Wyatt Mack <wyattmack91@gmail.com>
2020-02-20 12:51:40 -0700

    Updated to HTML

--- submission.php
+++ submission.php
@@ -1,11 +1,39 @@
-<?php
-echo 'These are the GET key:value pairs:<br />';
-print_r('fruit:' . htmlspecialchars($_GET['fruit']) . '<br />');
-print_r('asr:' . htmlspecialchars($_GET['asr']) . '<br /><br /> OR <br /><br />');
-print_r($_GET);
-
-echo '<br /><br />These are the POST key:value pairs:<br />';
-print_r('show:' . htmlspecialchars($_POST['show']) . '<br />');
-print_r('game:' . htmlspecialchars($_POST['game']) . '<br /><br /> OR <br /><br />');
-print_r($_POST);
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Results Page</title>
+    </head>
+    <body>
+        <div>
+            <p>
+                These are the GET key:value pairs:<br />
+                <?php print_r('fruit:' . htmlspecialchars($_GET['fruit']));?>
+                <br />
+                <?php print_r('asr:' . htmlspecialchars($_GET['asr']));?>
+            </p>
+            <p>
+                OR
+            </p>
+            <p>
+                <?php print_r($_GET);?>
+            </p>
+        </div>
+        <div>
+            <!-- <br>
+            <br>
+            <br>
+            <br> -->
+            <p>
+                These are the POST key:value pairs:<br />
+                <?php print_r('fruit:' . htmlspecialchars($_POST['show']));?>
+                <br />
+                <?php print_r('asr:' . htmlspecialchars($_POST['game']));?>
+            </p>
+            <p>
+                OR
+            </p>
+            <p>
+                <?php print_r($_POST);?>
+        </div>
+    </body>
+</html>
